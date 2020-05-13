40aed5906f55d186735252e40b610c96a5babfae
badruti94 <badruakfm@gmail.com>
2020-05-13 16:50:24 +0700

    1

---
+++ assets/data.js
@@ -0,0 +1,115 @@
+const projek = [{
+        nama: 'Quran',
+        sc: 'https://github.com/badruti94/quran',
+        web: 'https://badruti94.github.io/quran/',
+        desc: 'Aplikasi Al-Qur’an dan Doa dengan menggunakan API dari quran cloud',
+        dibuat: 'html, css, boostrap, jquery',
+        status: 'selesai',
+        foto: './assets//img/1.png'
+    },
+    {
+        nama: 'Movie Info',
+        sc: 'https://github.com/badruti94/movie_info',
+        web: '#',
+        desc: 'Aplikasi pencarian informasi film dengan memakai API dari omdb',
+        dibuat: 'html, css, bootstrap, javascript, dan webpack',
+        status: 'selesai',
+        foto: './assets//img/2.png'
+    },
+    {
+        nama: 'Pantau Covid',
+        sc: 'https://github.com/badruti94/pantaucovid',
+        web: 'https://badruti94.github.io/pantaucovid/',
+        desc: 'Aplikasi tracking covid-19 menggunakan API kawalcorona',
+        dibuat: 'html, css, bootstrap, jquery',
+        status: 'ditunda',
+        foto: './assets//img/3.png'
+    },
+    {
+        nama: 'Pengenalan Hewan',
+        sc: 'https://github.com/badruti94/pengenalan_hewan',
+        web: '#',
+        desc: 'Aplikasi pengenalan hewan dengan Progressive Web Aps',
+        dibuat: 'html, css, materialize, jquery',
+        status: 'selesai',
+        foto: './assets//img/4.png'
+    },
+    {
+        nama: 'Puasa',
+        sc: 'https://github.com/badruti94/puasa',
+        web: 'https://badruti94.github.io/puasa/',
+        desc: 'Aplikasi untuk mengetahui sisa hari ramadhan',
+        dibuat: 'template css dan jquery',
+        status: 'selesai',
+        foto: './assets//img/5.png'
+    },
+    {
+        nama: 'Web Programming',
+        sc: 'https://github.com/badruti94/school',
+        web: '#',
+        desc: 'html css sederhana',
+        dibuat: 'html dan css',
+        status: 'selesai',
+        foto: './assets//img/6.png'
+    },
+    {
+
+        nama: 'School',
+        sc: 'https://github.com/badruti94/school',
+        web: '#',
+        desc: 'halaman statis web sekolah sederhana',
+        dibuat: 'html, css, dan boostrap',
+        status: 'selesai',
+        foto: './assets//img/7.png'
+    },
+    {
+
+        nama: 'Sihasper',
+        sc: '#',
+        web: 'https://sihasper.com/',
+        desc: 'Sistem Informasi Hasil Perikanan. Dibuat untuk lomba. Tentang Olshop yang fokus pada hasil perikanan. ',
+        dibuat: 'laravel, template admin, css, animate css, boostrap, jquery, vue js, mariadb.',
+        status: 'pengembangan',
+        foto: './assets//img/8.png'
+    },
+    {
+
+        nama: 'Sitik',
+        sc: 'https://github.com/badruti94/sitik',
+        web: '#',
+        desc: 'Sistem Informasi TIK. Sistem Informasi untuk mengajukan email dan website kepada bidang TIK yang nantinya akan diproses lebih lanjut.',
+        dibuat: 'Laravel, template admin, dan bootstrap, mariadb',
+        status: 'selesai',
+        foto: './assets//img/9.png'
+    },
+    {
+
+        nama: 'Sipuh',
+        sc: 'https://github.com/badruti94/sipuh',
+        web: '#',
+        desc: 'Sistem Informasi Pengabsenan. Untuk mengelola data pengabsenan pada pengajian.',
+        dibuat: 'Laravel, template admin, dan bootstrap, mariadb',
+        status: 'dihentikan',
+        foto: './assets//img/10.png'
+    },
+    {
+
+        nama: 'Siperpus',
+        sc: 'https://github.com/badruti94/siperpus',
+        web: '#',
+        desc: 'Sistem Informasi Perpustakaan. Untuk mengelola data perpustakaan.',
+        dibuat: 'php7, template admin, dan bootstrap, mariadb',
+        status: 'selesai',
+        foto: './assets//img/11.png'
+    },
+    {
+
+        nama: 'Sitik Hasna',
+        sc: 'https://github.com/badruti94/sitikhasna',
+        web: '#',
+        desc: 'Sistem Informasi TIK. Sistem Informasi untuk mengajukan email.',
+        dibuat: 'php7, template admin, dan bootstrap, mariadb',
+        status: 'selesai',
+        foto: './assets//img/12.png'
+    }
+]
