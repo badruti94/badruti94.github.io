929004ba54e852fbaf3da1d732bc61a1502d9043
Badru <badruakfm@gmail.com>
2022-12-12 02:47:59 +0700

    add simple-stackoverflow-clone to projects

--- data.js
+++ data.js
@@ -43,12 +43,23 @@ const projects = [
         id: 5,
         title: 'Dumblink',
         image: 'dumblink.png',
-        description: 'Web App that generate web page contains links to social media, etc.',
+        description: 'Web App that generate web page contains links to social media, etc. Made with React and Node JS',
         githubs: [
             'https://github.com/badruti94/dumblink',
             'https://github.com/badruti94/dumblink-be',
         ],
         web: 'https://dumblink.vercel.app/'
+    },
+    {
+        id: 6,
+        title: 'Simple Stackoverflow Clone',
+        image: 'simple-stackoverflow-clone.png',
+        description: 'Situs untuk bertanya terkait coding, mirip seperti stackoverflow. Dibuat dengan React dan Node JS dengan Express JS sebagai frameworknya juga PostgreSQL sebagai databasenya',
+        githubs: [
+            'https://github.com/badruti94/stackoverflow-clone',
+            'https://github.com/badruti94/simple-stackoverflow-clone-be',
+        ],
+        web: 'https://stackoverflow-clone-kohl.vercel.app/'
     },
 
 ]
